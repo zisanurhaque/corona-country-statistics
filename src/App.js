7fa606e8934f21e5593e3a7b3b4935092ae21544
Zisanur Haque <mdzisanulhaque2000@gmail.com>
2022-02-18 08:06:44 +0600

    18-2-2022 1st

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-function App() {
+const App = () => {
+
+  const [countries, setCountries] = useState([])
+  const [global, setGlobal] = useState({})
+  const [country, setCountry] = useState({})
+  const [input, setInput] = useState('')
+
+  const handleInput = (e) => {
+    e.preventDefault()
+    const value = input.toLowerCase()
+    const update = countries.find((item) => {
+      return item.Country.toLowerCase() === value
+    })
+    if(update){
+      setCountry(update)
+    }else{
+      setCountry({})
+    }
+  }
+
+  useEffect(() => {
+
+    axios.get("https://api.covid19api.com/summary").then(response => setGlobal(response.data.Global))
+    axios.get("https://api.covid19api.com/summary").then(response => setCountries(response.data.Countries))
+
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+
+      <div className="rows">
+        <form onSubmit={handleInput}>
+          <input type="search" placeholder="Type full country name" onChange={(e) => setInput(e.target.value)}/>
+          <button>Search</button>
+        </form>
+      </div>
+
+      <div className="country">
+        <h3>{Object.keys(country).length === 0 ? "Global" : country.Country}</h3>
+        <p>{Object.keys(country).length === 0 ? global?.Date : country.Date}</p>
+      </div>
+
+      <div className="rows">
+        <div className="box">
+          <h5>New Confirmed</h5>
+          <h3>{Object.keys(country).length === 0 ? global?.NewConfirmed : country?.NewConfirmed}</h3>
+        </div>
+        <div className="box">
+          <h5>Total Confirmed</h5>
+          <h3>{Object.keys(country).length === 0 ? global?.TotalConfirmed : country?.TotalConfirmed}</h3>
+        </div>
+      </div>
+
+      <div className="rows">
+        <div className="box">
+          <h5>New Death</h5>
+          <h3>{Object.keys(country).length === 0 ? global?.NewDeaths : country?.NewDeaths}</h3>
+        </div>
+        <div className="box">
+          <h5>Total Death</h5>
+          <h3>{Object.keys(country).length === 0 ? global?.TotalDeaths : country?.TotalDeaths}</h3>
+        </div>
+      </div>
+
+      <div className="rows">
+        <div className="box">
+          <h5>New Recovered</h5>
+          <h3>{Object.keys(country).length === 0 ? global?.NewRecovered : country?.NewRecovered}</h3>
+        </div>
+        <div className="box">
+          <h5>Total Recovered</h5>
+          <h3>{Object.keys(country).length === 0 ? global?.TotalRecovered : country?.TotalRecovered}</h3>
+        </div>
+      </div>
+
+      <div className="rows">
+        <a rel="noreferrer noopener" target="_blank" href="https://api.covid19api.com/">api.covid19api.com</a><a rel="noreferrer noopener" target="_blank"href="https://zisanurhaque.netlify.app">Zisanur Haque</a>
+      </div>
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './style.css';
+import './responsive.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
